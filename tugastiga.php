6ad4e1e85db153b675524ab6b1cd013e6aa65f15
QoriHaidirAlam1994 <39036593+QoriHaidirAlam1994@users.noreply.github.com>
2018-05-08 22:23:30 +0700

    Update

--- tugastiga.php
+++ tugastiga.php
@@ -1,20 +1,81 @@
-
 <?php
-function randStrGen($len=300){
-	$result = "";
-	$chars = "abcdefghijklmnopqrstuwxyz0123456789";
-	$charArray = str_split($chars);
-	for($i = 0; $i < $len; $i++){
-		$randItem = array_rand($charArray);
-		$result .= "".$charArray[$randItem];
-	}
-	return $result;
+function randomPrefix($length)
+{
+    $random = "";
+    srand((double) microtime() * 1000000);
+    
+    $data = "AbcDE123IJKLMN67QRSTUVWXYZ";
+    $data .= "aBCdefghijklmn123opq45rs67tuv89wxyz";
+    $data .= "0FGH45OP89";
+    
+    for ($i = 0; $i < $length; $i++) {
+        $random .= substr($data, (rand() % (strlen($data))), 1);
+    }
+    
+    return $random;
 }
-?>
 
 
-<?php
-	$randstr = randStrGen(16);
-	echo $randstr;
-?>
+function randomPrefix2($length)
+{
+    $random = "";
+    srand((double) microtime() * 1000000);
+    
+    $data = "AbcDE123IJKLMN67QRSTUVWXYZ";
+    $data .= "aBCdefghijklmn123opq45rs67tuv89wxyz";
+    $data .= "0FGH45OP89";
+    
+    for ($i = 0; $i < $length; $i++) {
+        $random .= substr($data, (rand() % (strlen($data))), 1);
+    }
+    
+    return $random;
+}
+
+
+
+function randomPrefix3($length)
+{
+    $random = "";
+    srand((double) microtime() * 1000000);
+    
+    $data = "AbcDE123IJKLMN67QRSTUVWXYZ";
+    $data .= "aBCdefghijklmn123opq45rs67tuv89wxyz";
+    $data .= "0FGH45OP89";
+    
+    for ($i = 0; $i < $length; $i++) {
+        $random .= substr($data, (rand() % (strlen($data))), 1);
+    }
+    
+    return $random;
+}
+
+
+function randomPrefix4($length)
+{
+    $random = "";
+    srand((double) microtime() * 1000000);
+    
+    $data = "AbcDE123IJKLMN67QRSTUVWXYZ";
+    $data .= "aBCdefghijklmn123opq45rs67tuv89wxyz";
+    $data .= "0FGH45OP89";
+    
+    for ($i = 0; $i < $length; $i++) {
+        $random .= substr($data, (rand() % (strlen($data))), 1);
+    }
+    
+    return $random;
+}
+
+
+
+echo "<b>Menampilkan 300 serial number:</b><br />";
+for ($i = 1; $i <= 300; $i++) {
+    $b = randomPrefix2(4);
+    $a = randomPrefix(4);
+    $c = randomPrefix3(4);
+    $d = randomPrefix4(4);
+    echo '' . $a . '-' . $b . '-' . $c . '-' . $d . '<BR />';
+}
+
 
